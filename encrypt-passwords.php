547904f945e522a028c16ae6fc898bffcd5c9c19
k-kilgore <bucyrustech@hotmail.com>
2014-04-15 13:33:00 -0400

    Update encrypt-passwords.php
    fixed formatting

--- encrypt-passwords.php
+++ encrypt-passwords.php
@@ -2,16 +2,25 @@
 // Include the phpass library
 require_once('phpass-0.3/PasswordHash.php');
  
-// Initialize the hasher without portable hashes (this is more secure)
-$hasher = new PasswordHash(8, false);
+// Initialize hasher without portable hashes (this is more secure)
+$hasher = new PasswordHash(7, false);
  
-// Hash the password.  $hashedPassword will be a 60-character string.
-$hashedPassword = $hasher->HashPassword('my super cool password');
- 
-// You can now safely store the contents of $hashedPassword in your database!
- 
-// Check if a user has provided the correct password by comparing what they typed with our hash
+// declare password in a variable (in the next example we will pull it from a SQL table)
+$KalensPassword = ('this said something else but its not for the internet to know <3');
+
+// Hash the password.  $hashedPassword will be a 60-character string
+// that can be safely stored in a MYSQL database.
+$hashedPassword = $hasher->HashPassword($KalensPassword);
+
+// example of the CheckPassword function, we need to pull the password
+// data from the MYSQL table or else this entire script is a useless parlour trick
+
+//fail example
 $hasher->CheckPassword('the wrong password', $hashedPassword); // false
- 
-$hasher->CheckPassword('my super cool password', $hashedPassword); // true
+//success example
+if ($hasher->CheckPassword('this said something else but its not for the internet to know <3', $hashedPassword)) {
+	echo "The password hash is successful.";
+	}
+	
+echo "<p>The password hash is: \"$hashedPassword\"</p>";
 ?>
